e5a6f52917b979d9d84b4afe9b8c3ef0f47ca093
Jeffrey Fiddler <jeffnv@gmail.com>
2013-07-11 18:55:57 -0700

    added a way to add rooms

--- game.rb
+++ game.rb
@@ -1,6 +1,10 @@
-class dungeon
+class Dungeon
   
   attr_accessor :player
+  
+  def add_room(ref, name, desc, cnxns)
+    @rooms << Room.new(ref, name, desc, cnxns)
+  end
   
   def initialize(p_name)
     @player = Player.new(p_name)
@@ -28,3 +32,7 @@ end
 
 
 puts "welcome to the dungeon"
+
+dung = Dungeon.new('sarah')
+dung.add_room(:mens_room, "Mens Room", "A smelly bathroom, TP on the ground.", {:west => :womens_room})
+dung.add_room(:womens_room, "Womens Room", "Clean and pristine. They must use the men's room.", {:east => :mens_room})
