0a393917d0ef8123b1942277bae8cbd13f6e2998
Jeffrey Fiddler <jeffnv@gmail.com>
2013-07-14 08:06:54 -0700

    improvements to game, now for nav

--- game.rb
+++ game.rb
@@ -6,12 +6,25 @@ class Dungeon
     @rooms << Room.new(ref, name, desc, cnxns)
   end
   
+  def start(location)
+    @player.location = location
+    show_current_description
+  end
+  
+  def show_current_description
+    puts find_room_in_dungeon(@player.location).full_description
+  end
+  
+  def find_room_in_dungeon(reference)
+    @rooms.detect{|room| room.reference == reference}
+  end
+  
   def initialize(p_name)
     @player = Player.new(p_name)
     @rooms = []
   end
   
-  class player
+  class Player
     attr_accessor :name, :location
     
     def initialize(p_name)
@@ -28,6 +41,11 @@ class Dungeon
       @description = description
       @connections = connections
     end
+    
+    def full_description
+      "#{@name}\n\nYou are in #{description}"
+    end
+  end
 end
 
 
@@ -36,3 +54,4 @@ puts "welcome to the dungeon"
 dung = Dungeon.new('sarah')
 dung.add_room(:mens_room, "Mens Room", "A smelly bathroom, TP on the ground.", {:west => :womens_room})
 dung.add_room(:womens_room, "Womens Room", "Clean and pristine. They must use the men's room.", {:east => :mens_room})
+dung.start(:mens_room)
